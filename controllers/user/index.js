96726f1e26ae251cc09a8e9af9ab65d5bd9d0cdb
Vladislav <lezver1994@gmail.com>
2023-09-06 19:15:31 +0300

    hw05 - done

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -3,5 +3,13 @@ const { register } = require("./register");
 const { current } = require("./current");
 const { logout } = require("./logout");
 const { updateSubscription } = require("./updateSubscription");
+const { updateAvatar } = require("./updateAvatar");
 
-module.exports = { login, register, current, logout, updateSubscription };
+module.exports = {
+	login,
+	register,
+	current,
+	logout,
+	updateSubscription,
+	updateAvatar,
+};
